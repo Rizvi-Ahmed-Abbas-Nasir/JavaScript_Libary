8b2c4997836608627b056ffb171c8f4d60285e86
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2022-02-27 23:54:10 +0530

    abbas

--- Javascript1.js
+++ Javascript1.js
@@ -9,5 +9,32 @@ function myFunction()
   }
 
 }
-
+// Function call
 myFunction("Shama","Fatima");
+
+//Declare your variable here 
+var myGloble = 10;
+
+function funct1()
+{
+  // Assign 5 to oopsGloble here
+  oopsGloble = 5;
+
+}
+
+// only change code above this line 
+function fun2()
+{
+  var output = "";
+  if(typeof myGloble != "undefined"){
+    output += "myGloble: "+myGloble; 
+  }
+  if(typeof oopsGloble != "undefined"){
+    output += "oopsGloble: "+oopsGloble;
+  }
+  console.log(output);
+
+}
+// Function Call
+funct1();
+fun2();
